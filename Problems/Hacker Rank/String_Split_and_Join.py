dc67054822e9b387b226aefa6dca1bfb7332976b
ZeekJager <zachabera1999@gmail.com>
2026-01-28 15:18:04 +0300

    new problems

---
+++ Problems/Hacker Rank/String_Split_and_Join.py
@@ -0,0 +1,9 @@
+def split_and_join(line):
+    a = line.split()
+    result=""
+    result="-".join(a)
+    return(result)
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print(result)

fca5d16eedde573b7a7a7cc81ad1ed96f697cc10
ZeekJager <zachabera1999@gmail.com>
2026-02-04 23:40:57 +0300

    problems solved

---
+++ Problems/Hacker Rank/List_Comprehensions.py
@@ -0,0 +1,14 @@
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+    
+    for i in range(x*y*z):
+        for
+
+        for
+
+        for
+        
+    output=[i for i in range(x*y*z) output[i]=[i,]]

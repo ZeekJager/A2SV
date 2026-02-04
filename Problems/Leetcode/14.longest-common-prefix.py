b090e765e4e5e7cdee24772f917e3fb312173c9b
ZeekJager <zachabera1999@gmail.com>
2026-02-04 23:42:18 +0300

    Update

--- Problems/Leetcode/14.longest-common-prefix.py
+++ Problems/Leetcode/14.longest-common-prefix.py
@@ -1,10 +1,3 @@
-#
-# @lc app=leetcode id=14 lang=python3
-#
-# [14] Longest Common Prefix
-#
-
-# @lc code=start
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
         result=''

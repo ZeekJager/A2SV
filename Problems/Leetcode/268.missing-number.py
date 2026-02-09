8655ded565e207a5aba561c5ffff251243286d4f
ZeekJager <zachabera1999@gmail.com>
2026-02-10 01:09:13 +0300

    Another One

---
+++ Problems/Leetcode/268.missing-number.py
@@ -0,0 +1,18 @@
+#
+# @lc app=leetcode id=268 lang=python3
+#
+# [268] Missing Number
+#
+
+# @lc code=start
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        for i in range(len(nums)+1):
+            if i in nums:
+                continue
+            else:
+                return i
+
+        
+# @lc code=end
+

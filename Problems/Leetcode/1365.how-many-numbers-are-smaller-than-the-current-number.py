96cd18332043996a788bee3e5cf0c4d4f24731e4
ZeekJager <zachabera1999@gmail.com>
2026-02-12 14:43:36 +0300

    How many numbers are smaller than the current Number

--- Problems/Leetcode/1365.how-many-numbers-are-smaller-than-the-current-number.py
+++ Problems/Leetcode/1365.how-many-numbers-are-smaller-than-the-current-number.py
@@ -7,6 +7,14 @@
 # @lc code=start
 class Solution:
     def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
-        
+        ans=[]
+        for i in nums:
+            count =0
+            for j in nums:
+                if j < i:
+                    count+=1
+            ans.append(count)
+                
+        return ans
 # @lc code=end
 

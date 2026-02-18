bd8daad0a32ffbbcf9adbf6ee804f1af432f2471
ZeekJager <zachabera1999@gmail.com>
2026-02-18 23:28:09 +0300

    Find the Number of Distinct Colors Among the Balls

--- Problems/Leetcode/3160.find-the-number-of-distinct-colors-among-the-balls.py
+++ Problems/Leetcode/3160.find-the-number-of-distinct-colors-among-the-balls.py
@@ -5,8 +5,23 @@
 #
 
 # @lc code=start
+from collections import defaultdict
 class Solution:
     def queryResults(self, limit: int, queries: List[List[int]]) -> List[int]:
-        
+        res=[]
+        ballmap={}
+        c= defaultdict(list)
+        for ball, color in queries:
+            if ball in ballmap:
+                c[ballmap[ball]].remove(ball)
+                if not c[ballmap[ball]]:
+                    del c[ballmap[ball]]
+            ballmap[ball]=color
+            c[color].append(ball)
+            res.append(len(c))
+        return res
+            
+
+                    
 # @lc code=end
 
